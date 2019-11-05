040443f78988b709ba69f37d660971a7bce36094
Guy SHARONY <gsharony@e2r10p18.42.fr>
2019-11-05 11:48:55 +0100

    Key detectiong

--- main.c
+++ main.c
@@ -6,11 +6,26 @@
 /*   By: gsharony <gsharony@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 15:55:26 by gsharony          #+#    #+#             */
-/*   Updated: 2019/11/05 11:28:22 by gsharony         ###   ########.fr       */
+/*   Updated: 2019/11/05 11:48:11 by gsharony         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
+
+int		ft_key(int key, void *p)
+{
+	if (key == 123)
+		printf("Left\n");
+	else if (key == 124)
+		printf("Right\n");
+	else if (key == 125)
+		printf("Down\n");
+	else if (key == 126)
+		printf("Up\n");
+	else if (key == 53)
+		exit(0);
+	return (0);
+}
 
 int		main(void)
 {
@@ -21,6 +36,7 @@ int		main(void)
 	printf("\n___ Coloring map ___\n");
 	ft_fill(win, W, H, 750);
 	ft_text(win, W/2, H/2, 0, "Hello World");
+	mlx_key_hook(win, ft_key, 0);
 	mlx_loop(mlx);
 	return (EXIT_SUCCESS);
 }
