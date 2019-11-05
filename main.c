61f41b3abf19336373350cb0c238e326c0c61bee
Guy SHARONY <gsharony@e2r10p18.42.fr>
2019-11-05 12:20:43 +0100

    Creating area

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: gsharony <gsharony@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 15:55:26 by gsharony          #+#    #+#             */
-/*   Updated: 2019/11/05 12:17:11 by gsharony         ###   ########.fr       */
+/*   Updated: 2019/11/05 12:20:02 by gsharony         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,7 @@ int		main(void)
 	if ((win = mlx_new_window(mlx, W, H, "Hello World")) == NULL)
 		return (EXIT_FAILURE);
 	printf("\n___ Coloring map ___\n");
-	ft_fill(win, W, H, ft_color(255, 0, 0));
-	ft_text(win, W/2, H/2, 0, "Hello World");
+	ft_fill(win, W, H, ft_color(255, 255, 255));
 	ft_event(win, 0);
 	mlx_loop(mlx);
 	return (EXIT_SUCCESS);
