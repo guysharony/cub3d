d9a0cc52925d072a18f612d585133664fc204f84
Guy SHARONY <gsharony@e2r13p8.42.fr>
2019-11-28 14:02:27 +0100

    4 axes moves

--- game/event.c
+++ game/event.c
@@ -6,12 +6,11 @@
 /*   By: gsharony <gsharony@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/25 07:25:45 by gsharony          #+#    #+#             */
-/*   Updated: 2019/11/28 08:17:06 by gsharony         ###   ########.fr       */
+/*   Updated: 2019/11/28 14:01:47 by gsharony         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/main.h"
-#include <stdio.h>
 
 int		move(t_env *e)
 {
@@ -33,6 +32,20 @@ int		move(t_env *e)
 			e->player.pos.x -= e->player.dir.x * moveSpeed;
 		if(e->map[(int)(e->player.pos.x)][(int)(e->player.pos.y - e->player.dir.y * moveSpeed)] == '0')
 			e->player.pos.y -= e->player.dir.y * moveSpeed;
+	}
+	if (e->keys.t_0 == 1)
+	{
+		if(e->map[(int)(e->player.pos.x + e->player.dir.x * moveSpeed)][(int)(e->player.pos.y)] == '0')
+			e->player.pos.x -= e->player.dir.y * moveSpeed;
+		if(e->map[(int)(e->player.pos.x)][(int)(e->player.pos.y + e->player.dir.y * moveSpeed)] == '0')
+			e->player.pos.y += e->player.dir.x * moveSpeed;
+	}
+	if (e->keys.t_2 == 1)
+	{
+		if(e->map[(int)(e->player.pos.x - e->player.dir.x * moveSpeed)][(int)(e->player.pos.y)] == '0')
+			e->player.pos.x += e->player.dir.y * moveSpeed;
+		if(e->map[(int)(e->player.pos.x)][(int)(e->player.pos.y - e->player.dir.y * moveSpeed)] == '0')
+			e->player.pos.y -= e->player.dir.x * moveSpeed;
 	}
 	if (e->keys.t_124 == 1)
 	{
