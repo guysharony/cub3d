24a838b607aa958e31a70a394003330ff62c18b2
Guy SHARONY <gsharony@e3r8p8.42.fr>
2020-01-24 07:51:38 +0100

    Correcting resolution oversize

--- game/setters/set_resolution.c
+++ game/setters/set_resolution.c
@@ -6,7 +6,7 @@
 /*   By: gsharony <gsharony@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/17 13:45:28 by gsharony          #+#    #+#             */
-/*   Updated: 2020/01/17 14:01:48 by gsharony         ###   ########.fr       */
+/*   Updated: 2020/01/24 07:51:12 by gsharony         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,10 @@
 void	set_resolution(t_env *e)
 {
 	e->resolution[0] = ft_atoi(e->game->resolution[1]);
+	if (e->resolution[0] > 2560)
+		e->resolution[0] = 2560;
 	e->resolution[1] = ft_atoi(e->game->resolution[2]);
+	if (e->resolution[1] > 1440)
+		e->resolution[1] = 1440;
 	e->resolution[2] = e->resolution[0] * e->resolution[1];
 }
